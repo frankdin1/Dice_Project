68874ec8e109467825739fe02d367b947620ff12
frankdin <frankdin@yahoo.com>
2020-10-14 18:47:16 -0500

    added functionality to change dice image to match dice roll

--- dice.js
+++ dice.js
@@ -1,7 +1,12 @@
 //declare our variables
-var scores, roundScore, activePlayer, dice, currentPlayerScore, diceRoll;
+var scores, roundScore, activePlayer, dice, currentPlayerScore, diceRoll, diceImg;
+diceImg = document.querySelector(".dice");
 
+function gameInit(){
+	diceImg.style.display = "none";
+}
 
+gameInit();
 diceRoll = document.querySelector(".btn-roll")
 
 scores = [0,0];//global score array  for both players
@@ -13,5 +18,8 @@ currentPlayerScore = document.querySelector("#current-" + activePlayer);
 diceRoll.addEventListener("click", function(){
 	dice = Math.floor(Math.random()*6) + 1;// random number for dice roll
 	currentPlayerScore.innerHTML = dice;//change the value of player 1's round score
+	diceImg.style.display = "block";
+	diceImg.src = "dice-" + dice + ".jpg";
 	console.log(dice)
 })
+
