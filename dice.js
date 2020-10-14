7a47e1ccd50ddd7253f0341e4f4f644f7e28125c
frankdin <frankdin@yahoo.com>
2020-10-14 18:09:50 -0500

    used type coercion to dynamically change active player

--- dice.js
+++ dice.js
@@ -1,16 +1,17 @@
 //declare our variables
 var scores, roundScore, activePlayer, dice, currentPlayerScore, diceRoll;
 
-currentPlayerScore = document.querySelector("#score-0")
+
 diceRoll = document.querySelector(".btn-roll")
 
 scores = [0,0];//global score array  for both players
 roundScore = 0;// round score for each player
-
-
+activePlayer = 1;
+currentPlayerScore = document.querySelector("#current-" + activePlayer);
 
 
 diceRoll.addEventListener("click", function(){
 	dice = Math.floor(Math.random()*6) + 1;// random number for dice roll
 	currentPlayerScore.innerHTML = dice;//change the value of player 1's round score
+	console.log(dice)
 })
