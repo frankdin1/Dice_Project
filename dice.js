e3b7fa05075c6c3289bfa5c9ad0c3ef815163d40
frankdin <frankdin@yahoo.com>
2020-10-14 19:34:19 -0500

    initialized all scores to zero;assigned var to global and current score for each player;commented out click function

--- dice.js
+++ dice.js
@@ -1,25 +1,34 @@
 //declare our variables
-var scores, roundScore, activePlayer, dice, currentPlayerScore, diceRoll, diceImg;
+var scores, roundScore, activePlayer, dice, player1CurrentScore,player2CurrentScore, player1GlobalScore,playe21GlobalScore, diceRoll, diceImg;
 diceImg = document.querySelector(".dice");
+scores = [0,0];//global score array  for both players
+roundScore = 0;// round score for each player
+activePlayer = [0,1];
+player1CurrentScore = document.querySelector("#current-" + activePlayer[0]);
+player2CurrentScore = document.querySelector("#current-" + activePlayer[1]);
+player1GlobalScore = document.querySelector("#score-" + activePlayer[0]);
+player2GlobalScore = document.querySelector("#score-" + activePlayer[1]);
+
 
 function gameInit(){
 	diceImg.style.display = "none";
+	player1CurrentScore.innerHTML = 0;
+	player2CurrentScore.innerHTML = 0;
+	player1GlobalScore.innerHTML = 0;
+	player2GlobalScore.innerHTML = 0;
 }
 
 gameInit();
 diceRoll = document.querySelector(".btn-roll")
 
-scores = [0,0];//global score array  for both players
-roundScore = 0;// round score for each player
-activePlayer = 1;
-currentPlayerScore = document.querySelector("#current-" + activePlayer);
 
 
-diceRoll.addEventListener("click", function(){
-	dice = Math.floor(Math.random()*6) + 1;// random number for dice roll
-	currentPlayerScore.innerHTML = dice;//change the value of player 1's round score
-	diceImg.style.display = "block";
-	diceImg.src = "dice-" + dice + ".jpg";
-	console.log(dice)
-})
+
+// diceRoll.addEventListener("click", function(){
+// 	dice = Math.floor(Math.random()*6) + 1;// random number for dice roll
+// 	;//change the value of player's round score
+// 	diceImg.style.display = "block";
+// 	diceImg.src = "dice-" + dice + ".jpg";
+// 	console.log(dice)
+// })
 
